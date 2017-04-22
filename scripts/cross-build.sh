1741b3389f0e2a71b33bc77ab667d1a4b98fbca9
Julian Langschaedel <meta.rb@gmail.com>
2017-04-22 05:43:48 +0200

    change cross-build.sh setup

--- scripts/cross-build.sh
+++ scripts/cross-build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+export GOPATH=$HOME/go
+
 cd "$(dirname ${BASH_SOURCE[0]})"
 
 set -x
@@ -9,15 +11,15 @@ cd builds/tmp
 version=alpha-v7
 githash=$(git rev-parse HEAD)
 
-ssh qemu "/opt/bin/xgo -v -x  -ldflags '-X main.AppVersion=$version -X main.AppGitHash=$githash' --targets=darwin/amd64,windows/386 github.com/lian/gdax-bookmap"
-scp qemu:gdax-bookmap-windows-4.0-386.exe gdax-bookmap.exe
+/opt/bin/xgo -v -x  -ldflags '-X main.AppVersion=$version -X main.AppGitHash=$githash' --targets=darwin/amd64,windows/386 ../../../
+mv gdax-bookmap-windows-4.0-386.exe gdax-bookmap.exe
 zip -r ../gdax-bookmap-win32.zip gdax-bookmap.exe
-rm gdax-bookmap.exe
+rm -f gdax-bookmap.exe
 
 cp -r ../../macOS-tmpl gdax-bookmap.app
-scp qemu:gdax-bookmap-darwin-10.6-amd64 gdax-bookmap.app/Contents/MacOS/gdax-bookmap
+cp gdax-bookmap-darwin-10.6-amd64 gdax-bookmap.app/Contents/MacOS/gdax-bookmap
 zip -r ../gdax-bookmap-osx.zip gdax-bookmap.app
-rm -r gdax-bookmap.app
+rm -rf gdax-bookmap.app gdax-bookmap-darwin-10.6-amd64
 
 cd ..
 echo VERSION gdax-bookmap $version-$githash > checksum.txt
